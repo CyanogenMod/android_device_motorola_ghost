bd2f4296fe380a6127072230d614ac94e62a5419
dhacker29 <davidhackerdvm@gmail.com>
2013-11-26 10:04:11 -0600

    init.recovery: Set brightness
    Change-Id: I788a0c39b67bc6b715ad04890f822e4e3fedcedf

--- msm8960dt.mk
+++ msm8960dt.mk
@@ -48,6 +48,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.qcom.rc \
+    init.recovery.qcom.rc \
     init.target.rc \
     ueventd.qcom.rc
 
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -56,6 +56,14 @@ LOCAL_SRC_FILES    := etc/init.qcom.wifi.sh
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.qcom.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.recovery.qcom.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.target.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
