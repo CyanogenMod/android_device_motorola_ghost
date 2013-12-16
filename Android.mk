1dbe508f9f45747b2bdef680c79191b58f8edfb1
dhacker29 <davidhackerdvm@gmail.com>
2013-12-16 16:48:51 -0600

    Move wpa_supplicant_overlay to family/add tdls_external_control=1
    Change-Id: I967de50eabcae04ef58901732b3ae17d05b462a5

--- Android.mk
+++ Android.mk
@@ -19,11 +19,20 @@
 # to only building on ARM if they include assembly. Individual makefiles
 # are responsible for having their own logic, for fine-grained control.
 
-LOCAL_PATH := $(call my-dir)
-
 ifeq ($(BOARD_VENDOR),motorola-msm8960)
 ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
 ifneq ($(filter xt1053 xt1055 xt1056 xt1058 xt1060 xt1080 xt1080m,$(TARGET_DEVICE)),)
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := wpa_supplicant_overlay.conf
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES := config/$(LOCAL_MODULE)
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/wifi
+include $(BUILD_PREBUILT)
+
 include $(call all-makefiles-under,$(LOCAL_PATH))
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
