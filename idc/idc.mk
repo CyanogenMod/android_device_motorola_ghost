0aa53db1c0f75cf2e6e10b8c1fd3edfde3e0fdee
dhacker29 <dhackerdvm@gmail.com>
2014-02-08 15:48:41 -0600

    Add idc/kl for obake
    Change-Id: Ieec5d19e8e05315e3d54e87cb2b9648b9f00ee73

---
+++ idc/idc.mk
@@ -0,0 +1,3 @@
+LOCAL_PATH := device/motorola/msm8960dt-common
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/idc/synaptics_dsx_i2c.idc:system/usr/idc/synaptics_dsx_i2c.idc
--- keylayout/keylayout.mk
+++ keylayout/keylayout.mk
@@ -1,4 +1,5 @@
 LOCAL_PATH := device/motorola/msm8960dt-common
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/keylayout/msp430sensorprocessor.kl:system/usr/keylayout/msp430sensorprocessor.kl \
+	$(LOCAL_PATH)/keylayout/synaptics_dsx_i2c.kl:system/usr/keylayout/synaptics_dsx_i2c.kl \
 	$(LOCAL_PATH)/keylayout/synaptics_rmi4_i2c.kl:system/usr/keylayout/synaptics_rmi4_i2c.kl
