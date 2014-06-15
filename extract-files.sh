c6aabf8578a56b3d52130b4f1ef8fe04b0b9ef77
dhacker29 <dhackerdvm@gmail.com>
2014-06-15 03:45:48 -0400

    proprietary-files: Organize by usage
    Change-Id: Ica0f6c89b22b185293db802fbca28f23eb37c534

--- extract-files.sh
+++ extract-files.sh
@@ -2,5 +2,9 @@
 
 export VENDOR=motorola
 export DEVICE=moto_msm8960dt
+# Comment these out if there is no common device tree for this device
+export COMMON_DEVICE=qcom-common
+export BOARD_VENDOR=motorola-qcom
+export TARGET_BOARD_PLATFORM=msm8960
 
-../qcom-common/extract-files.sh $@
+../$COMMON_DEVICE/extract-files.sh $@
