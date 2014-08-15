2a11d40f08d8ffc2a27f24f1b5375471e366bf39
Ethan Chen <intervigil@gmail.com>
2014-08-14 18:09:23 -0700

    ghost: Make device config stand alone

--- device.mk
+++ device.mk
@@ -14,129 +14,40 @@
 # limitations under the License.
 #
 
-$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-LOCAL_PATH := device/motorola/moto_msm8960dt
+$(call inherit-product, vendor/motorola/ghost/ghost-vendor.mk)
 
-# moto_msm8960dt specific overlay
+# Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-
-PRODUCT_LOCALES := en_US
-PRODUCT_LOCALES += xhdpi
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 # Permissions
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml
 
-# Audio
-PRODUCT_PACKAGES += \
-    audio_policy.msm8960 \
-    audio.primary.msm8960
-
-# HAL
-PRODUCT_PACKAGES += \
-    copybit.msm8960 \
-    gps.msm8960 \
-    gralloc.msm8960 \
-    hwcomposer.msm8960 \
-    keystore.msm8960 \
-    lights.MSM8960 \
-    memtrack.msm8960 \
-    power.msm8960
-
-# WIFI
-PRODUCT_PACKAGES += wcnss_service
-
-# Symlinks
-PRODUCT_PACKAGES += \
-    mbhc.bin \
-    wcd9310_anc.bin \
-    WCNSS_qcom_wlan_nv_calibration.bin \
-    WCNSS_qcom_wlan_nv_regulatory.bin \
-    WCNSS_qcom_wlan_nv_calibration_0x3.bin \
-    WCNSS_qcom_wlan_nv_calibration_0x4.bin \
-    WCNSS_qcom_wlan_nv_calibration_0x5.bin \
-    WCNSS_qcom_wlan_nv_calibration_0x6.bin \
-    WCNSS_qcom_wlan_nv_regulatory_0x3.bin \
-    WCNSS_qcom_wlan_nv_regulatory_0x4.bin \
-    WCNSS_qcom_wlan_nv_regulatory_0x5.bin \
-    WCNSS_qcom_wlan_nv_regulatory_0x6.bin
-
-# Utilities
-PRODUCT_PACKAGES += \
-    charge_only_mode \
-    mkfs.f2fs \
-    fsck.f2fs \
-    fibmap.f2fs
-
-# EGL config
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/config/egl.cfg:system/lib/egl/egl.cfg
-
-# GPS configuration
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf
-
-# Sap/location secuity configuration file
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/config/sap.conf:system/etc/sap.conf \
-    $(LOCAL_PATH)/config/sec_config:system/etc/sec_config
-
-# Wifi
-PRODUCT_COPY_FILES += \
-    kernel/motorola/msm8960dt-common/drivers/staging/prima/firmware_bin/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
-    kernel/motorola/msm8960dt-common/drivers/staging/prima/firmware_bin/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
-    $(LOCAL_PATH)/wlan/cal_files/WCNSS_qcom_wlan_nv_calibration.bin:system/etc/firmware/wlan/prima/cal_files/WCNSS_qcom_wlan_nv_calibration.bin \
-    $(LOCAL_PATH)/wlan/cal_files/WCNSS_qcom_wlan_nv_calibration_0x1.bin:system/etc/firmware/wlan/prima/cal_files/WCNSS_qcom_wlan_nv_calibration_0x1.bin \
-    $(LOCAL_PATH)/wlan/cal_files/WCNSS_qcom_wlan_nv_calibration_0x2.bin:system/etc/firmware/wlan/prima/cal_files/WCNSS_qcom_wlan_nv_calibration_0x2.bin \
-    $(LOCAL_PATH)/wlan/cal_files/WCNSS_qcom_wlan_nv_regulatory.bin:system/etc/firmware/wlan/prima/cal_files/WCNSS_qcom_wlan_nv_regulatory.bin \
-    $(LOCAL_PATH)/wlan/cal_files/WCNSS_qcom_wlan_nv_regulatory_0x1.bin:system/etc/firmware/wlan/prima/cal_files/WCNSS_qcom_wlan_nv_regulatory_0x1.bin \
-    $(LOCAL_PATH)/wlan/cal_files/WCNSS_qcom_wlan_nv_regulatory_0x2.bin:system/etc/firmware/wlan/prima/cal_files/WCNSS_qcom_wlan_nv_regulatory_0x2.bin
-
-# Ramdisk
-PRODUCT_PACKAGES += \
-    fstab.qcom \
-    init.qcom.rc \
-    init.recovery.qcom.rc \
-    init.target.rc \
-    ueventd.qcom.rc
-
-# Init scripts
-PRODUCT_PACKAGES += \
-    init.mmi.boot.sh \
-    init.mmi.touch.sh \
-    init.qcom.post_boot.sh \
-    init.qcom.sh \
-    init.qcom.wifi.sh
-
-# TWRP
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/twrp.fstab:recovery/root/etc/twrp.fstab
-
-# Audio configuration
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/config/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/config/snd_soc_msm_2x:system/etc/snd_soc_msm/snd_soc_msm_2x
-
-# Charger mode setup
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/moto_com.sh:system/bin/moto_com.sh
-
-# Media config
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/config/media_profiles.xml:system/etc/media_profiles.xml \
-    $(LOCAL_PATH)/config/media_codecs.xml:system/etc/media_codecs.xml
-
-# QCOM Display
+# System Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     hw.trueMirrorSupported=1
 
-# Media
 PRODUCT_PROPERTY_OVERRIDES += \
     media.stagefright.enable-player=true \
     media.stagefright.enable-http=true \
@@ -149,7 +60,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.mot.vr_source_dsp=1 \
     af.resampler.quality=255
 
-# Misc
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.usb.mtp=0x2e62 \
     ro.usb.mtp_adb=0x2e63 \
@@ -168,27 +78,288 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.audio.fluencetype=fluence \
     ro.cwm.forbid_format="/fsg,/firmware,/persist,/modem,/boot"
 
-# Opengles version 3
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196608
 
-# QCOM Display
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=320
 
-# LTE, CDMA, GSM/WCDMA
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.default_network=10 \
-    telephony.lteOnCdmaDevice=1 \
     persist.radio.mode_pref_nv10=1 \
     persist.radio.no_wait_for_card=1 \
     persist.radio.dfr_mode_set=1 \
     persist.rmnet.mux=disabled \
     persist.radio.eons.enabled=1
 
-$(call inherit-product, device/motorola/qcom-common/qcom-common.mk)
-$(call inherit-product, device/motorola/moto_msm8960dt/idc/idc.mk)
-$(call inherit-product, device/motorola/moto_msm8960dt/keylayout/keylayout.mk)
-$(call inherit-product, device/motorola/qcom-common/keylayout/keylayout.mk)
-$(call inherit-product, device/motorola/qcom-common/modules/nfc/nfc.mk)
-$(call inherit-product, vendor/motorola/moto_msm8960dt/moto_msm8960dt-vendor.mk)
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.egl.hw=1 \
+    debug.sf.hw=1 \
+    debug.composition.type=dyn \
+    persist.hwc.mdpcomp.enable=true \
+    debug.mdpcomp.logs=0 \
+    debug.enabletr=0
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    com.qc.hardware=true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.audio.fluence.mode=endfire \
+    persist.audio.vr.enable=false \
+    persist.audio.handset.mic=digital \
+    ro.qc.sdk.audio.ssr=false
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.bluetooth.hfp.ver=1.6 \
+    ro.qualcomm.bluetooth.sap=true \
+    ro.qualcomm.bt.hci_transport=smd \
+    ro.bluetooth.request.master=true \
+    ro.bluetooth.remote.autoconnect=true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    lpa.decode=true \
+    qcom.hw.aac.encoder=true \
+    af.resampler.quality=255 \
+    persist.audio.lowlatency.rec=false
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.qc.sub.rstrtlvl=3 \
+    persist.sys.qc.sub.rdump.on=1 \
+    persist.sys.qc.sub.rdump.max=20
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    rild.libpath=/system/lib/libril-qc-qmi-1.so
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ril.subscription.types=NV,RUIM \
+    keyguard.no_require_sim=true \
+    ro.use_data_netmgrd=true \
+    ro.ril.transmitpower=true \
+    persist.radio.apm_sim_not_pwdn=1 \
+    persist.radio.call_type=1 \
+    ro.config.vc_call_vol_steps=7 \
+    ro.modem.no_wdog_chk=1
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.timed.enable=true
+
+# Screen density
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+
+# Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    audio.primary.msm8960 \
+    audio.r_submix.default \
+    audio.usb.default \
+    audio_policy.msm8960 \
+    libaudio-resampler
+
+PRODUCT_PACKAGES += \
+    mbhc.bin \
+    wcd9310_anc.bin
+
+# Audio configuration
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/config/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/config/snd_soc_msm_2x:system/etc/snd_soc_msm/snd_soc_msm_2x
+
+# Charger
+PRODUCT_PACKAGES += \
+    charger \
+    charger_res_images
+
+# CRDA
+PRODUCT_PACKAGES += \
+    crda \
+    linville.key.pub.pem \
+    regdbdump \
+    regulatory.bin
+
+# Display
+PRODUCT_PACKAGES += \
+    copybit.msm8960 \
+    gralloc.msm8960 \
+    hwcomposer.msm8960 \
+    libgenlock \
+    memtrack.msm8960
+
+# Filesystem
+PRODUCT_PACKAGES += \
+    e2fsck \
+    fibmap.f2fs \
+    fsck.f2fs \
+    make_ext4fs \
+    mkfs.f2fs \
+    resize2fs \
+    setup_fs
+
+# GPS
+PRODUCT_PACKAGES += \
+    gps.msm8960
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/config/sap.conf:system/etc/sap.conf
+
+# IRSC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/config/sec_config:system/etc/sec_config
+
+# Keystore
+PRODUCT_PACKAGES += \
+    keystore.msm8960
+
+# Lights
+PRODUCT_PACKAGES += \
+    lights.msm8960
+
+# Media
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/config/media_profiles.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/config/media_codecs.xml:system/etc/media_codecs.xml
+
+# Motorola
+PRODUCT_PACKAGES += \
+    aplogd \
+    charge_only_mode \
+
+# NFC
+PRODUCT_PACKAGES += \
+    libnfc \
+    libnfc_jni \
+    Nfc \
+    Tag \
+    com.android.nfc_extras
+
+ifeq ($(TARGET_BUILD_VARIANT),user)
+    PRODUCT_COPY_FILES += $(LOCAL_PATH)/config/nfcee_access.xml:system/etc/nfcee_access.xml
+else
+    PRODUCT_COPY_FILES += $(LOCAL_PATH)/config/nfcee_access_debug.xml:system/etc/nfcee_access.xml
+endif
+
+PRODUCT_COPY_FILES += \
+    frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml
+
+# OMX
+PRODUCT_PACKAGES += \
+    libdashplayer \
+    libOmxVdec \
+    libOmxVenc \
+    libOmxAacEnc \
+    libOmxAmrEnc \
+    libOmxEvrcEnc \
+    libOmxQcelp13Enc \
+    libstagefrighthw \
+    qcmediaplayer
+
+PRODUCT_BOOT_JARS += \
+    qcmediaplayer
+
+# Power HAL
+PRODUCT_PACKAGES += \
+    power.msm8960
+
+# QRNGD
+PRODUCT_PACKAGES += \
+    qrngd
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    init.qcom.rc \
+    init.qcom.usb.rc \
+    init.recovery.qcom.rc \
+    init.target.rc \
+    ueventd.qcom.rc
+
+PRODUCT_PACKAGES += \
+    init.class_main.sh \
+    init.crda.sh \
+    init.qcom.bt.sh \
+    init.qcom.class_core.sh \
+    init.qcom.coex.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.efs.sync.sh \
+    init.qcom.fm.sh \
+    init.qcom.mdm_links.sh \
+    init.qcom.modem_links.sh \
+    init.qcom.syspart_fixup.sh \
+    init.qcom.thermal_conf.sh \
+    init.qcom.usb.sh
+
+PRODUCT_PACKAGES += \
+    init.mmi.boot.sh \
+    init.mmi.touch.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.sh \
+    init.qcom.wifi.sh
+
+PRODUCT_PACKAGES += \
+    thermald-8960.conf \
+    thermald-ghost.conf \
+    thermal-engine-8226.conf
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/scripts/mount_pds.sh:system/bin/mount_pds.sh
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/moto_com.sh:system/bin/moto_com.sh
+
+# Support
+PRODUCT_PACKAGES += \
+    libcurl \
+    libxml2
+
+# Torch
+PRODUCT_PACKAGES += \
+    Torch
+
+# USB
+PRODUCT_PACKAGES += \
+    com.android.future.usb.accessory
+
+# Wifi
+PRODUCT_PACKAGES += \
+    libQWiFiSoftApCfg \
+    wcnss_service
+
+PRODUCT_PACKAGES += \
+    hostapd.accept \
+    hostapd_default.conf \
+    hostapd.deny \
+    p2p_supplicant_overlay.conf \
+    wpa_supplicant_overlay.conf
+
+PRODUCT_PACKAGES += \
+    WCNSS_qcom_wlan_nv.bin \
+    WCNSS_qcom_wlan_nv_calibration.bin \
+    WCNSS_qcom_wlan_nv_regulatory.bin \
+    WCNSS_qcom_wlan_nv_calibration_0x3.bin \
+    WCNSS_qcom_wlan_nv_calibration_0x4.bin \
+    WCNSS_qcom_wlan_nv_calibration_0x5.bin \
+    WCNSS_qcom_wlan_nv_calibration_0x6.bin \
+    WCNSS_qcom_wlan_nv_regulatory_0x3.bin \
+    WCNSS_qcom_wlan_nv_regulatory_0x4.bin \
+    WCNSS_qcom_wlan_nv_regulatory_0x5.bin \
+    WCNSS_qcom_wlan_nv_regulatory_0x6.bin
+
+PRODUCT_COPY_FILES += \
+    kernel/motorola/msm8960dt-common/drivers/staging/prima/firmware_bin/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
+    kernel/motorola/msm8960dt-common/drivers/staging/prima/firmware_bin/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
+    $(LOCAL_PATH)/wlan/cal_files/WCNSS_qcom_wlan_nv_calibration.bin:system/etc/firmware/wlan/prima/cal_files/WCNSS_qcom_wlan_nv_calibration.bin \
+    $(LOCAL_PATH)/wlan/cal_files/WCNSS_qcom_wlan_nv_calibration_0x1.bin:system/etc/firmware/wlan/prima/cal_files/WCNSS_qcom_wlan_nv_calibration_0x1.bin \
+    $(LOCAL_PATH)/wlan/cal_files/WCNSS_qcom_wlan_nv_calibration_0x2.bin:system/etc/firmware/wlan/prima/cal_files/WCNSS_qcom_wlan_nv_calibration_0x2.bin \
+    $(LOCAL_PATH)/wlan/cal_files/WCNSS_qcom_wlan_nv_regulatory.bin:system/etc/firmware/wlan/prima/cal_files/WCNSS_qcom_wlan_nv_regulatory.bin \
+    $(LOCAL_PATH)/wlan/cal_files/WCNSS_qcom_wlan_nv_regulatory_0x1.bin:system/etc/firmware/wlan/prima/cal_files/WCNSS_qcom_wlan_nv_regulatory_0x1.bin \
+    $(LOCAL_PATH)/wlan/cal_files/WCNSS_qcom_wlan_nv_regulatory_0x2.bin:system/etc/firmware/wlan/prima/cal_files/WCNSS_qcom_wlan_nv_regulatory_0x2.bin
