1c9d25c2445570fb701b4f8896890476ee23de48
Ethan Chen <intervigil@gmail.com>
2014-09-12 14:10:58 -0700

    ghost: Don't set custom vmalloc, remove unused zcache flag
    * Unsure why Motorola set 400MB, but it appears not to be necessary.
    * Use Linux standard 128MB
    * zcache isn't generally available on ARM kernels.
    
    Change-Id: Ia3a415a888cceaef71b6bcf2cd4988ec32415b30

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,7 +43,7 @@ TARGET_CPU_VARIANT := krait
 TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 maxcpus=2 vmalloc=400M androidboot.write_protect=0 zcache
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 maxcpus=2 androidboot.write_protect=0
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02200000 --dt $(LOCAL_PATH)/dt.img
