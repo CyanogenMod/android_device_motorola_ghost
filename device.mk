856896aba038b2288fb3e44770e5838630d16a90
Brandon McAnsh <brandon.mcansh@gmail.com>
2014-11-16 20:05:06 -0500

    Use native google media codecs instead of local copies
    * Also include telephony codecs
    
    Change-Id: I7ebc184bd1c09c5c098da7d8f4560fa7dc768d9a
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- device.mk
+++ device.mk
@@ -134,8 +134,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/configs/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-    $(LOCAL_PATH)/configs/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
 # Motorola
 PRODUCT_PACKAGES += \
