0de43a339a531ecce7b17f446e45bd3ac1ae043c
Ethan Chen <intervigil@gmail.com>
2014-12-15 00:35:05 -0800

    ghost: Move mount_pds to bin folder
    Change-Id: I362fa87432fc1016b0e729093e7d68ad26aadb31

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -42,6 +42,7 @@ LOCAL_MODULE       := mount_pds.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/mount_pds.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 # Init scripts
