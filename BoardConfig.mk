8a64c1a0b57d6d4c9e6bde620814bd3cd97b1f51
Ethan Chen <intervigil@gmail.com>
2014-12-18 00:44:56 -0800

    ghost: Remove unnecessary SEPOLICY_UNION
    Change-Id: I28a4a0a26082a42187c29902914e264d1e0c2ca5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -135,22 +135,6 @@ TARGET_USERIMAGES_USE_F2FS := true
 BOARD_SEPOLICY_DIRS += \
     device/motorola/ghost/sepolicy
 
-BOARD_SEPOLICY_UNION += \
-    device.te \
-    file_contexts \
-    file.te \
-    init.te \
-    init_shell.te \
-    keystore.te \
-    location.te \
-    mm-pp-daemon.te \
-    mm-qcamerad.te \
-    mpdecision.te \
-    platform_app.te \
-    rmt_storage.te \
-    thermal-engine.te \
-    ueventd.te
-
 # USB
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 
