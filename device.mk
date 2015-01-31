81ed6250c84020436b50d4b68c30b288adfdf523
Skrilax_CZ <skrilax@gmail.com>
2015-01-31 21:45:20 +0100

    ghost: Use product package overlay
    * Higher priority over device package overlay
    
    Change-Id: Id3e5b7d398d2c579f58954495e84cbd1b037d50d

--- device.mk
+++ device.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, vendor/motorola/ghost/ghost-vendor.mk)
 
 # Overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+PRODUCT_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Keylayout
 PRODUCT_COPY_FILES += \
