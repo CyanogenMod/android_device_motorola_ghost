12d0ac35d1303014e5263816e7b11032439d3c8c
Ethan Chen <intervigil@gmail.com>
2016-05-09 16:41:07 -0700

    ghost: Convert to standalone init library
    Change-Id: I3a26cb05f1d8e9d0ef14cbeee43509b8a7d8ca6f

--- init/init_ghost.cpp
+++ init/init_ghost.cpp
@@ -34,9 +34,8 @@
 #include "log.h"
 #include "util.h"
 
-#include "init_msm.h"
-
 #define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]))
+#define ISMATCH(a, b) (!strncmp((a), (b), PROP_VALUE_MAX))
 
 static void set_cmdline_properties()
 {
@@ -90,8 +89,7 @@ static void cdma_properties(const char *default_sub, const char *op_numeric,
     property_set("telephony.rilV7NeedCDMALTEPhone", "true");
 }
 
-void init_msm_properties(unsigned long msm_id, unsigned long msm_ver,
-        char *board_type)
+void vendor_load_properties()
 {
     char platform[PROP_VALUE_MAX];
     char radio[PROP_VALUE_MAX];
@@ -100,10 +98,6 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver,
     char bootdevice[PROP_VALUE_MAX];
     char devicename[PROP_VALUE_MAX];
     int rc;
-
-    UNUSED(msm_id);
-    UNUSED(msm_ver);
-    UNUSED(board_type);
 
     rc = property_get("ro.board.platform", platform);
     if (!rc || !ISMATCH(platform, ANDROID_TARGET))
